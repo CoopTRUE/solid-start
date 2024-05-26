51c492c7a4b8748d983d0a4106d6988024a3849b
CoopTRUE <coopdog4545@gmail.com>
2024-05-25 17:26:34 -0700

    final linting touches

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
 import Counter from '~/components/Counter'
+import { cn } from '~/lib/utils'
 
 export default function Home() {
   return (
-    <main class="flex container flex-col items-center justify-center gap-4">
+    <main class={cn('container flex flex-col items-center justify-center gap-4')}>
       <h1 class="text-7xl">Hello world!</h1>
       <Counter />
     </main>
